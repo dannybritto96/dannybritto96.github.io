80af51011181c284dd8866933a83ba91e9c79f71
Danie Britto Kuthalane <dannybritto96@gmail.com>
2018-02-23 18:15:47 +0530

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,15 +1,33 @@
 import os
 import sys
 import threading
+import commands
 filename = sys.argv[1]
 lock = threading.Lock()
 f = open(filename,"r")
+q = open("/tmp/spectre_results.csv","a+")
+p = open("/tmp/scandata","r")
 x = []
 for line in f:
     line = line.rstrip('\n')
     x.append(line)
 def execute(s):
-    os.system("./spectre.sh {0}".format(s))
+    cmd1 = "ssh -o PasswordAuthentication=no -o ConnectTimeout=1 -o StrictHostKeyChecking=no {0} exit".format(s)
+    z = os.system(cmd1)
+    if z == 0:
+        cmd2 = "scp /export/home/bs/NagiosCommand/NagiosAgent/libexec/os-scan.sh {0}:/export/home/bs/NagiosCommand/NagiosAgent/libexec/os-scan.sh".format(s)
+        os.system(cmd2)
+        cmd3 = "scp /export/home/bs/NagiosCommand/NagiosAgent/libexec/spectre-meltdown-checker.sh {0}:/export/home/bs/NagiosCommand/NagiosAgent/libexec/spectre-meltdown-checker.sh".format(s)
+        os.system(cmd3)
+        cmd4 = "ssh -t {0} sudo /export/home/bs/NagiosCommand/NagiosAgent/libexec/os-scan.sh meltdown > /tmp/scandata".format(s)
+        os.system(cmd4)
+        lock.acquire()
+        scandata = commands.getoutput("cat /tmp/scandata |grep -v '#'|tr '\n' ';'")
+        cmdz = "echo -e '{1};{0}' >> /tmp/spectre_results.csv".format(scandata,s)
+        lock.acquire()
+        os.system(cmdz)
+        lock.release()
+
 for item in x:
     t = threading.Thread(target = execute, args = (item,))
     t.start()
@@ -18,3 +36,4 @@ while threading.active_count()>1:
     pass
 else:
     f.close()
+
